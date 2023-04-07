b2a67cd4e1b3a980f9175e18ed7a545b7e7a7890
Omar Elrefaei <Elrefaei.Omar@gmail.com>
2023-04-07 10:36:04 -0400

    Updates

--- investigation-logs.md
+++ investigation-logs.md
@@ -10,9 +10,31 @@
 - was able to trigger faliures in solve1
 	- Little hard to trigger at orders lower than 3, but found at order=2, n=4, m=3 (seeds 652 and 727)
 	- order 2 with n, m 6 or up is easy
+...
+...
+*Heavy call with Michel*
+*After lots of debugging, he decided to investigate cond(lhs)*
+- Turns out yes, lhs in solve1 "test" is sometimes close to singular so inverting it is not very accurate
+**Conclusion:** we assume the problem is with the testing ldiv a not-very-inverable matrix and not our functions 
+- but after seemingly solving the problem 2 things comeup (one: not very easy to filter out all true-poisives without a ton of false-positives...this doesn't seem to be exactly the right createria but close)
+- Two: that doesn't really solve the problem with generalized_sylvester_solver! since there is no ldiv there
+	(with solve1 the \approx values where actually close, that is not the case with gss)
+	- I have few ideas: 1: intercept args to solve1 call in a failing gss and see if solve1 is actually failing there
+	- replace the solve1 call in gss with a julia ldiv version like the tests
+	- 
+- Observation: gss, number of failing seeds increase with n (equality faliure is big)
 
 
 
 
+
+replacing norm with opnorm in isapprox
+pdiv instead of ldiv
+
+
+
+cond2 -> opnorm2 -> svdvals
+> Note in pinv:  For inverting dense ill-conditioned matrices in a least-squares sense, rtol =
+  sqrt(eps(real(float(one(eltype(M)))))) is recommended.
 
 # Log
--- investigation.jl
+++ investigation.jl
@@ -1,7 +1,17 @@
+
 function test_repetedly(;func, seed=false, n_tries=20, verbose=true)
 	pass_fail_log::Vector{Bool} = []
 	for i in 1:n_tries
-		push!(pass_fail_log, func(seed))
+		try
+			r = func(seed)
+			push!(pass_fail_log, r)		
+		catch ex
+			if ex == SingularException
+				if seed != false return nothing # if seeded, skip the rest
+				else continue end
+			else rethrow()
+		end
+	end
 	end
 	# true, false, or missing
 	results = consistent_or_not(pass_fail_log) 
@@ -16,34 +26,42 @@ end
 
 function scan_seeds(;func, seeds::UnitRange{Int64}, n_tries=20)
 	pass_fail_dict = Dict()
-	Threads.@threads for seed in seeds
+	# Threads.@threads
+	for seed in seeds
 		res = test_repetedly(func=func, seed=seed, n_tries=n_tries, verbose=false)
 		pass_fail_dict[seed] = res
 	end
 
-	# print the seeds that consistently passed or was inconsistent, the rest has failed
 	inconsistent_seeds = filter(p->ismissing(p.second), pass_fail_dict) |> sort 
 	passed_seeds = filter(p->isequal(true)(p.second), pass_fail_dict) |> sort
 	failed_seeds = filter(p->isequal(false)(p.second), pass_fail_dict) |> sort
-	println("Passed seeds: ", [p.first for p in passed_seeds])
-	println("Failed seeds: ", [p.first for p in failed_seeds])
-	println("Inconsistent seeds: ", [p.first for p in inconsistent_seeds])
+	# printing block
+	println("\nPassing seeds: $(length(passed_seeds))/$(length(seeds))")
+	0<length(passed_seeds)<15 ? println("  =>	", [p.first for p in passed_seeds]) : nothing
+	println("Failing seeds: $(length(failed_seeds))/$(length(seeds))")
+	0<length(failed_seeds)<15 ? println("  =>	", [p.first for p in failed_seeds]) : nothing
+	println("Inconsistent Seeds: $(length(inconsistent_seeds))/$(length(seeds))")
+	0<length(inconsistent_seeds)<15 ? println("  =>	", [p.first for p in inconsistent_seeds]) : nothing
+	if length(failed_seeds)+length(inconsistent_seeds) == 0 print("✨ Nice, no faliures or inconsistencies ✨") end
 	if length(passed_seeds) == length(seeds) print("✨ All $(length(seeds)) seeds Passed ✨") end
 	return passed_seeds
-end
-
+end	
 
 function consistent_or_not(list::Vector{Bool})
-	if all(isequal(true), list) return true
+	if isempty(list) return nothing
+	elseif all(isequal(true), list) return true
 	elseif all(isequal(false), list) return false
 	else return missing end
 end
 
 
+
+######### Wrapper functions to test and trigger faliure #########
+
 function gss_test(seed)
 	if seed != false Random.seed!(seed) end
 	n1 = 4
-	n2 = 3
+	n2 = 4
 	order = 2
 	a = randn(n1,n1)
 	b = randn(n1,n1)
@@ -55,8 +73,19 @@ function gss_test(seed)
 	c_orig = copy(c)
 	d_orig = copy(d)
 
+	# GeneralizedSylvesterSolver.flag = true
+	# @info "Seed: $seed"
 	d = generalized_sylvester_solver!(a, b, c, copy(d_orig), order, deepcopy(ws))
-	a_orig*d + b_orig*d*kron(c_orig,c_orig) ≈ d_orig
+	# @show GeneralizedSylvesterSolver.flag
+	
+	# a_orig*d + b_orig*d*kron(c_orig,c_orig) ≈ d_orig
+	lhs1 = a_orig*d + b_orig*d*kron(c_orig,c_orig)
+	rhs1 = d_orig
+
+	lhs1 ≈ rhs1 
+
+
+
 end
 
 function linsolv_test(seed)
@@ -91,8 +120,14 @@ function solve1_test(seed)
     d = randn(m*n^order)
     alpha = randn()
     beta = randn()
-    t = lu(a\b).U
-    s = lu(c).U
+
+    #only real eigenvalues
+    # t = lu(a\b).U
+    # s = lu(c).U
+   
+    t = schur(a\b).T
+    s = schur(c).T
+
     t2 = QuasiUpperTriangular(t*t)
     s2 = QuasiUpperTriangular(s*s)
     t = QuasiUpperTriangular(t)
@@ -103,10 +138,22 @@ function solve1_test(seed)
     ws = GeneralizedSylvesterWs(m, m, n, order)
 	d = randn(m*n^order)
 	d_orig = copy(d)
+
 	GeneralizedSylvesterSolver.solve1!(r,order,t,t2,s,s2,d,ws)
-	d_target = (I(m*n^order) + kron(kron_power(s',order),t))\d_orig
-	d ≈ d_target
+	
+	lhs = (I(m*n^order) + kron(kron_power(s',order),r*t))
+	# cond_num = abs(cond(lhs)) # 9 is too lax, could go 10 (np 11 is too tight sometimes lets 1/1000)
+	# cond_num = abs(condskeel(lhs)) # 1e9 (1e8 to catch seed 1288 but let way too many false-pos)
+	cond_num = abs(cond(lhs)) 
+	if cond_num > 1e9
+        @info "Seed $seed: condition number $cond_num is too high, skipping..."
+        throw(SingularException)
+    end
     
+    d_target = lhs * d_orig	
+    # isapprox(d, d_target, norm=norm)
+	d ≈ d_target
+
 end
 
 function addition_demo_test(seed)
