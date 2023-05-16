aa3d9834209e259dd6e03b2a86cc3b3ea6fe54fd
Omar Elrefaei <Elrefaei.Omar@gmail.com>
2023-05-16 13:24:24 -0400

    Better DataFrames. Unified Constructor and better names

--- investigation.jl
+++ investigation.jl
@@ -28,31 +28,18 @@ function scan_seeds(;func, seeds::UnitRange{Int64}, n_tries=1)
 	lk = ReentrantLock()
 	pass_fail_dict = Dict()
 	df = DataFrame()
-	df[!, :seed] .= 0
-	df[!, :OrigProb] .= false
-	df[!, :Ana] .= false
-	df[!, :Ana_opnorm] .= false
-	df[!, :res_mateq_vs_gss] .= false
-	df[!, :res_mateq_vs_ana] .= false
-	df[!, "norm(GSS-Ana)"] .= 0.0
-	# df[!, "opnorm(GSS-Ana)"] .= 0.0
-	df[!, "norm(GSS-OrigProb)"] .= 0.0
-	# df[!, "opnorm(GSS-OrigProb)"] .= 0.0
-	df[!, "norm(Ana-OrigProb)"] .= 0.0
-	# df[!, "opnorm(Ana-OrigProb)"] .= 0.0
-	df[!, "condNum_ana"] .= 0.0
 
 	prog = Progress(length(seeds))
 	Threads.@threads for seed in seeds
 		res = gss_test(seed)
 		lock(lk) do
-			push!(df, res)
+			append!(df, res, cols=:union)
 		end
 		next!(prog)
 	end
 
-	passed_seeds = subset(df, :OrigProb) 
-	failed_seeds = subset(df, :OrigProb=>x->x.==false)
+	passed_seeds = subset(df, :gss_vs_orgPrb) 
+	failed_seeds = subset(df, :gss_vs_orgPrb=>x->x.==false)
 	println("\nPassing seeds: $(length(eachrow(passed_seeds)))/$(length(seeds))")
 	println("Failing seeds: $(length(eachrow(failed_seeds)))/$(length(seeds))")
 	0<length(eachrow(failed_seeds))<15 ? println("  =>	", failed_seeds.seed) : nothing
@@ -64,6 +51,8 @@ function gss_test(seed)
 	n1 = 4
 	n2 = 4
 	order = 4
+	rtol = sqrt(eps(1.0))*order
+	rtol2 = sqrt(eps(1.0))*n1^order
 	a = randn(Float64,n1,n1); b = randn(Float64,n1,n1); c = randn(Float64,n2,n2); d = randn(Float64,n1,n2^order)
 	ws = GeneralizedSylvesterWs(n1,n1,n2,order)
 	a_orig = copy(a); b_orig = copy(b); c_orig = copy(c); d_orig = copy(d)
@@ -71,21 +60,21 @@ function gss_test(seed)
 	
 	_a = kron_power(c_orig, order)
 	lhs1 = b_orig*d*_a + a_orig * d 
-	res = lhs1 ≈ d_orig
+	gss_vs_orgPrb = isapprox(lhs1, d_orig)
+	gss_vs_orgPrb_rtol = isapprox(lhs1, d_orig, rtol=rtol)
 
 	A = (kron(I(n2^order),a_orig) + kron(kron_power(c_orig', order),b_orig))
-	b = vec(d_orig)
-	sol2 = reshape(A\b, n1,n2^order)
+	sol2 = reshape(A\vec(d_orig), n1,n2^order)
 	# sol2 = reshape( solve(LinearProblem(A, b)).u, n1,n2^order)
 	res2 = isapprox(d, sol2)
-	res3 = isapprox(d, sol2, norm=opnorm)
+	res3 = isapprox(d, sol2, rtol=rtol)
+	res4 = isapprox(d, sol2, rtol=rtol2)
 	# A*_s ≈ b ? nothing : @show A*_s ≈ b
 
 	## subbing in the linAlg solution into the original problem
-	lhs2 = b_orig*d*_a + a_orig * sol2 
-	res3 = lhs2 ≈ d_orig
+	lhs2 = b_orig*sol2*_a + a_orig * sol2 
+	res_ana_vs_orgPrb = isapprox(lhs2, d_orig)
 
-	results = [res, res2, res3]
 	if res2 == false
 		# println("seed: $seed, res1 is $res, and res2 is $res2")
 		# @show lhs1, d_orig, lhs2, d, sol2
@@ -101,13 +90,30 @@ function gss_test(seed)
 	d_mateq = gsylv(b_orig, kron_power(c_orig,order), a_orig, 1.0, d_orig)
 	res_mateq_vs_gss = isapprox(d, d_mateq)
 	res_mateq_vs_ana = isapprox(sol2, d_mateq)
+	
+	lhs3 = b_orig*d_mateq*_a + a_orig * d_mateq 
+	res_matEq_vs_orgPrb = isapprox(lhs3, d_orig)
+	dict = OrderedDict(
+		:seed => seed,
+		:gss_vs_orgPrb => gss_vs_orgPrb,
+		:gss_vs_orgPrb_rtol => gss_vs_orgPrb_rtol,
+		:gss_vs_ana => res2,
+		:gss_vs_ana_rtol => res3,
+		:gss_vs_ana_rtol2 => res4,
+		:ana_vs_orgPrb => res_ana_vs_orgPrb,
+		:matEq_vs_gss => res_mateq_vs_gss,
+		:matEq_vs_ana => res_mateq_vs_ana,
+		:matEq_vs_orgPrb => res_matEq_vs_orgPrb,
+		Symbol("norm(GSS-OrgPrb)") => norm(lhs1-d_orig),
+		Symbol("norm(GSS-Ana)") => norm(d-sol2),
+		Symbol("norm(Ana-OrigProb)") => norm(lhs2-d_orig),
+		Symbol("norm(matEq-GSS)") => norm(d-d_mateq),
+		Symbol("norm(matEq-Ana)") => norm(sol2-d_mateq),
+		Symbol("norm(matEq-OrigProb)") => norm(lhs3-d_orig),
+		Symbol("cond(Ana)") => cond(A),
+		Symbol("cond(b&d)") => cond(b)+cond(d_orig),
+		)
 
-	# dict = Dict(:seed=>seed, :res1 => res, :res2=>res2)
-	dict = (seed, res, res2, res3, res_mateq_vs_gss, res_mateq_vs_ana,
-		norm(d-sol2),
-		norm(lhs1-d_orig),  
-		norm(lhs2-d_orig), 
-		cond(A))
 	# res3 is worst, and surprisingly not corolated with res2
 	return dict
 
